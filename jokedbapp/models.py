c1d0c6d3a713906579a343f9f5d362e1d8c4d0bf
ben o'steen <bosteen@gmail.com>
2015-05-21 10:31:43 +0000

    Updated models to reflect needs for the db

--- jokedbapp/models.py
+++ jokedbapp/models.py
@@ -1,15 +1,9 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Enum
 from sqlalchemy.orm import relationship, backref
 
 from database import Base
 
 from datetime import datetime
-
-ADMINROLE = 0
-TRANSCRIBERROLE = 1
-PUBLISHERROLE = 2
-
-MAPPING = {0: "admin", 1:"Transcriber", 2:"Publisher"}
 
 class User(Base):
   __tablename__ = 'users'
@@ -17,65 +11,170 @@ class User(Base):
   name = Column(String(50), unique=True)
   email = Column(String(120), unique=True)
   pw_hash = Column(String)
-  role = Column(Integer)
+  role = Column(Enum('admin','transcriber', 'publisher', name='role_types'))
+  transcriptions = relationship("Transcription", backref="edited")
+  biblio_records = relationship("Biblio", backref="creator")
+  joke_records = relationship("Joke", backref="publisher")
 
-  def __init__(self, name=None, email=None, role=TRANSCRIBERROLE, pw_hash = pw_hash):
+  def __init__(self, name=None, email=None, role='transcriber', pw_hash = pw_hash):
     self.name = name
     self.email = email
     self.role = role
     self.pw_hash = pw_hash
 
   def is_admin(self):
-    if self.role == 0:
+    if self.role == 'admin':
       return True
     else:
       return False
 
   def __repr__(self):
-    return '<User {0}, role={1}>'.format(self.name, MAPPING[self.role])
+    return '<User {0}, role={1}>'.format(self.name, self.role)
+
+class Biblio(Base):
+  __tablename__ = 'biblio'
+  id = Column(Integer, primary_key=True)
+  title = Column(String(70))
+  date = Column(String(20))
+  year = Column(Integer)
+  author = Column(String(30))
+  editor = Column(String(30))
+  publisher = Column(String(30))
+  city = Column(String(30))
+  country = Column(String(30))
+  gale = Column(String(30))
+  blshelfmark = Column(String(30))
+  shownby = Column(String(200))  # URL direct link perhaps?
+  rights = Column(String(60))
+  rightsholder= Column(String(60))
+  citation = Column(String)
+  itemtype = Column(Enum('periodical', 'book', name='pub_type'))
+  periodical_freq = Column(Enum('weekly', 'daily', 'fortnightly', 'monthly', 'annually', "NA", name='publication_rate'))
+  created_at = Column(DateTime)
+  creator_id = Column(Integer, ForeignKey('users.id'))
+  #creator = relationship(User, backref=backref('biblio_author', uselist=True))
+  transcription_sections = relationship("Transcription", backref="biblio")
+  
+  def __init__(self, title, date, year=0, author=None, editor=None, publisher=None, city=None, \
+                     country=None, gale=None, blshelfmark=None, shownby=None, rights=None, \
+                     rightsholder=None, itemtype=None, periodical_freq="NA", citation=None, record_creator = None):
+    self.title = title
+    self.date = date
+    self.year = year
+    self.author = author
+    self.editor = editor
+    self.publisher = publisher
+    self.city = city
+    self.country = country
+    self.gale = gale
+    self.blshelfmark = blshelfmark
+    self.shownby = shownby
+    self.rights = rights
+    self.rightsholder = rightsholder
+    self.itemtype = itemtype
+    self.periodical_freq = periodical_freq
+    self.citation = citation
+    #self.creator = record_creator
+    if record_creator:
+      self.creator_id = record_creator.id
+    self.created_at = datetime.now()
+
+  def __repl__(self):
+    return '<Biblio {0}({1}) shelfmark:{2}>'.format(self.title, self.date, self.blshelfmark)
+  
 
 class Transcription(Base):
   __tablename__ = 'transcriptions'
   id = Column(Integer, primary_key=True)
   from_id = Column(Integer)
+  raw = Column(String)
   text = Column(String)
   date_harvested = Column(DateTime)
+  parsed = Column(Integer)
   created_at = Column(DateTime)
-  by_user_id = Column(Integer, ForeignKey('users.id'))
-  by_user = relationship(User, backref=backref('editor', uselist=True))
+  edited_id = Column(Integer, ForeignKey('users.id'))
+  #by_user = relationship(User, backref=backref('editor', uselist=True))
   # TODO: Add citation metadata columns
+  pagestart = Column(Integer)
+  pageend = Column(Integer)
+  volume = Column(String(20))
+  article_title = Column(String(70))
+  biblio_id = Column(Integer, ForeignKey('biblio.id'))  
+  #biblio  = relationship(Biblio, backref=backref('transcriptions', uselist=True))
+  joke_children = relationship("Joke", backref="from_transcription")
 
-  def __init__(self, from_id=None, text=None, date_harvested=None, by_user = None):
+  def __init__(self, from_id=None, text=None, date_harvested=None, by_user = None, raw = None, parsed = 0, \
+                     biblio = None, article_title = None, volume = None, pagestart = 0, pageend = 0):
     self.from_id = from_id
     self.text = text
     self.date_harvested = date_harvested
-    self.by_user = by_user
-    self.by_user_id = by_user.id
+    #self.by_user = by_user
+
+    if by_user:
+      self.edited_id = by_user.id
+    else:
+      self.edited_id = None
+
+    if biblio:
+      self.biblio_id = biblio.id
+    else:
+      self.biblio_id = None
+
+    self.raw = raw
+    self.parsed = parsed
     self.created_at = datetime.now()
+    self.article_title = article_title
+    self.volume = volume
+    self.pagestart = pagestart
+    self.pageend = pageend    
 
   def __repr__(self):
-    return '<Transcription: {0} -- "{1}..." by {2}>'.format(self.id, self.text[:15], self.by_user)
+    if self.text:
+      return '<Transcription: {0} -- "{1}..." from {3}({4}) by {2}>'.format(self.id, self.text[:15], self.edited, self.article_title, self.biblio.date)
+    return '<Transcription: {0} -- from {2}({3}) by {1}>'.format(self.id, self.edited, self.article_title, self.biblio.date)
 
 class Joke(Base):
   __tablename__ = 'jokes'
   id = Column(Integer, primary_key=True)
   from_transcription_id = Column(Integer, ForeignKey('transcriptions.id'))
-  from_transcription = relationship(Transcription, backref=backref('transcription_src', uselist=False))
+  #from_transcription = relationship(Transcription, backref=backref('transcription_src', uselist=False))
   transcription_position = Column(Integer)
   text = Column(String)
+  title = Column(String)
+  tumblr_id = Column(String)
+  tweet_id = Column(String)
+  attribution = Column(String)
+  joketext = Column(String)
   #image_id = Column(Integer, ForeignKey('pictures.id'))
   #image = relationship(Picture, backref=backref('images', uselist=False))
   created_at = Column(DateTime, nullable=False)
+  published_at = Column(DateTime)
+  publisher_id = Column(Integer, ForeignKey('users.id'))
+  #published_by = relationship(User, backref=backref('publisher', uselist=True))
   
   def __init__(self, transcription = None, transcription_position = None, text = None, \
-               published_at = None, tumblr_id = None, tweet_id = None, published_by = None):
-    self.from_transcription = transcription
+               published_at = None, tumblr_id = None, tweet_id = None, published_by = None, \
+               title = None, attribution = None, joketext = None):
+    #self.from_transcription = transcription
+    if transcription != None:
+      self.from_transcription_id = transcription.id
     self.transcription_position = transcription_position
     self.text = text 
+    self.title = title 
+    self.attribution = attribution 
+    self.joketext = joketext
     self.created_at = datetime.now()
+    self.published_at = published_at
+    if published_by:
+      #self.published_by = published_by
+      self.published_by_id = published_by.id
+    self.tweet_id = tweet_id
+    self.tumblr_id = tumblr_id
 
   def __repr__(self):
-    return '<Joke: {0} -- "{1}..." by "{2}">'.format(self.id, self.text[:15], self.published_by)
+    if self.tumblr_id:
+      return '<Joke: {0} -- "{1}..." On tumblr: "{2}">'.format(self.id, self.text[:15], self.tumblr_id)
+    return '<Joke: {0} -- "{1}..." -- not published on web>'.format(self.id, self.text[:15])
 
 class Picture(Base):
   __tablename__ = 'pictures'
