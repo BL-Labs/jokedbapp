b88fba91a2879e58d03d78b6360a7bdd69eab12e
ben o'steen <bosteen@gmail.com>
2015-05-21 10:34:13 +0000

    refined image profiles

--- jokedbapp/image_profiles/__init__.py
+++ jokedbapp/image_profiles/__init__.py
@@ -1,3 +1,5 @@
 from basic_2x2 import LAYOUT as basic_layout, BACKGROUND_COLOUR as basic_bg, PARTS as basic_parts
+from basic_vertical import LAYOUT as basic_layoutV, BACKGROUND_COLOUR as basic_bgV, PARTS as basic_partsV
 
 BASIC = (basic_bg, basic_parts, basic_layout)
+BASICV = (basic_bgV, basic_partsV, basic_layoutV)
--- jokedbapp/image_profiles/basic_2x2.py
+++ jokedbapp/image_profiles/basic_2x2.py
@@ -1,22 +1,30 @@
 import cairo
 
-DEFAULT = {'font': 'DejaVu Serif', 'size':20.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1}
-BOLD = {'font': 'Georgia', 'size':20.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_BOLD, 'LINEHEIGHT':1}
-EMPHASIS = {'font': 'Georgia', 'size':20.0, 'style': cairo.FONT_SLANT_OBLIQUE, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1}
-TITLE = {'font': 'Georgia', 'size':25.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_BOLD, 'LINEHEIGHT':1}
+DEFAULT = {'font': 'American Typewriter', 'size':16.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1.1}
+DEFAULT_SMALL = {'font': 'American Typewriter', 'size':2.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1.1}
+BOLD = {'font': 'American Typewriter', 'size':16.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_BOLD, 'LINEHEIGHT':1}
+BOLD_SMALL = {'font': 'American Typewriter', 'size':12.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1}
+EMPHASIS = {'font': 'American Typewriter', 'size':16.0, 'style': cairo.FONT_SLANT_OBLIQUE, 'weight': cairo.FONT_WEIGHT_NORMAL, 'LINEHEIGHT':1}
+TITLE = {'font': 'American Typewriter', 'size':18.0, 'style': cairo.FONT_SLANT_NORMAL, 'weight': cairo.FONT_WEIGHT_BOLD, 'LINEHEIGHT':1.3}
 
 LAYOUT = {'_SIZE': (2,2), 'DEFAULT': (0,0), 'IMAGE': (1, 0), 'ATTRIB': (0,1)}
 
-BACKGROUND_COLOUR = (0.0, 0.0, 0.0, 1.0)   #RGBA
+BACKGROUND_COLOUR = (0.2, 0.2, 0.2, 1.0)   #RGBA
 
 TEXTS = {'DEFAULT': DEFAULT, 
          'BOLD': BOLD,
          'EMPHASIS': EMPHASIS,
-         'TITLE': TITLE,}
+         'TITLE': TITLE,
+         'DEFAULT_SMALL': DEFAULT_SMALL,
+         'BOLD_SMALL': BOLD_SMALL,}
 
-PARTS = {'DEFAULT': {'BACKGROUND': (1.0, 1.0, 1.0, 1.0), 'FOREGROUND': (0.0, 0.0, 0.0, 1.0), 
-                     'TEXT': TEXTS, 'TEXTWIDTH':30, "MARGIN":(15,50)},
-         'IMAGE': {'BACKGROUND': (1.0, 1.0, 1.0, 1.0), 'FOREGROUND': (0.0, 0.0, 0.0, 1.0), 
-                   'TEXT': TEXTS, 'TEXTWIDTH':None, "MARGIN":(15,50)},
-         'ATTRIB': {'BACKGROUND': (0.5, 0.1, 0.1, 1.0), 'FOREGROUND': (1.0, 1.0, 1.0, 1.0), 
-                   'TEXT': TEXTS, 'TEXTWIDTH':15, "MARGIN":(15,50)},}
+PARTS = {'DEFAULT': {'BACKGROUND': ((171.0/256.0), (140.0/256.0), (16.0/256.0), 1.0), 'FOREGROUND': (0.0, 0.0, 0.0, 1.0), 
+                     'TEXT': TEXTS, 'TEXTWIDTH':70, "MARGIN":(15,10)},
+         'IMAGE': {'BACKGROUND': ((158.0/256.0), (11.0/256.0), (15.0/256.0), 1.0), 'FOREGROUND': (0.0, 0.0, 0.0, 1.0), 
+                   'TEXT': TEXTS, 'TEXTWIDTH':None, "MARGIN":(5,5),
+                   'IMAGES': ["/home/ben/jokedb/jokedbapp/imgs/tophat.png",
+                             "/home/ben/jokedb/jokedbapp/imgs/facepalm.png",
+                             "/home/ben/jokedb/jokedbapp/imgs/mechcom.png", ]
+          },
+         'ATTRIB': {'BACKGROUND': ((158.0/256.0), (11.0/256.0), (15.0/256.0), 1.0), 'FOREGROUND': (1.0, 1.0, 1.0, 1.0),
+                   'TEXT': TEXTS, 'TEXTWIDTH':40, "MARGIN":(30,5)},}
